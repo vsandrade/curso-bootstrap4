fbca18780d7cf0a976791d83c440f408924f8386
Vinícius de Andrade <ozirispc@gmail.com>
2019-01-14 15:28:24 -0200

    Inserindo Arquivos Angular

---
+++ 01 - Projeto Cervejaria/js/appzeebeer.js
@@ -0,0 +1,34 @@
+var app = angular.module("appzeebeer", ["ngSanitize"]);
+app.controller("zeebeerCtrl", function ($scope) {
+	$scope.titulo = "Zee Beer";
+	$scope.cervejas = cervejas;
+
+	$scope.detalhar = function (cerveja) {
+		$scope.cervejaDetalhe = cerveja;
+		console.log(cerveja);
+	};
+});
+
+app.filter('cut', function () {
+	return function (value, wordwise, max, tail) {
+		if (!value) return '';
+
+		max = parseInt(max, 10);
+		if (!max) return value;
+		if (value.length <= max) return value;
+
+		value = value.substr(0, max);
+		if (wordwise) {
+			var lastspace = value.lastIndexOf(' ');
+			if (lastspace !== -1) {
+                  //Also remove . and , so its gives a cleaner result.
+                  if (value.charAt(lastspace-1) === '.' || value.charAt(lastspace-1) === ',') {
+                  	lastspace = lastspace - 1;
+                  }
+                  value = value.substr(0, lastspace);
+              }
+          }
+
+          return value + (tail || ' …');
+      };
+  });
---
+++ 01 - Projeto Cervejaria/js/bd.js
@@ -0,0 +1,80 @@
+var cervejas = [
+	{
+		'nome' : 'Fonte do Céu',
+		'tipo' : 'Cervejas Witbier',
+		'ingredientes' : 'Água, malte de cevada alemão, malte de trigo belga, especiarias, gengibre, lúpulos europeus e levedura',
+		'composicao' : 'ABV 4.7%. - IBU 14',
+		'descricao' : `As Witbiers são cervejas artesanais originalmente belgas, produzidas a partir do trigo, coentro e cascas de laranja, o que lhes confere um estilo  peculiar, pautado pela leveza e refrescância de seu sabor. O estilo Witbier foi criado há mais de 400 anos, mas havia desaparecido em torno dos anos 1950. Até que o cervejeiro belga Pierre Celi, na cidade de Hoegaarden, decidiu retomar a produção baseado em suas memórias de criança. Após a façanha do cervejeiro, o estilo voltou com força e graças a ele hoje temos inúmeras cervejarias produzindo as Wit Beer.
+					   <br><br>As cervejas Witbier, apesar de serem feitas com trigo, são um pouco diferente das Weiss. As Witbier são cervejas turvas, de cor amarelo palha, com espuma branca e cremosa. O aroma é geralmente adocicado e notas cítricas de laranja, provenientes das cascas de laranja que são usadas na fabricação. 
+					   <br><br>A nossa Fonte do Céu, além do cítrico da laranja, tem o sabor surpreendente do gengibre. Tornando-a única nesse estilo. É uma cerveja muito bonita, leve e ideal para se beber no verão. Nossa Fonte foi inspirada na Cachoeira Fonte do Céu, que fica na Gamboa. A visita vale a pena!`,
+		'img' : 'fontedoceu.png'
+
+	},
+	{
+		'nome' : 'IPAiÓ',
+		'tipo' : 'English IPA',
+		'ingredientes' : 'Água, malte de cevada alemão, malte de cevada belga, lúpulos europeus, lúpulos americanos e levedura',
+		'composicao' : 'ABV 6.1%. - IBU 71',
+		'descricao' : `IPA significa India Pale Ale, um estilo que veio da Índia. São cervejas amargas devido a concentração de lúpulos, extremamente aromáticas, refrescantes e fáceis de beber. Sua cor deve ser de dourado a acobreado. Apresentam um teor alcoólico que tradicionalmente vai de 5,5 a 7,5%. 
+					   <br><br>A história, ou lenda, é que os ingleses precisavam de cervejas com mais lúpulo e álcool para que ela aguentasse viagens mais longas, pois o lúpulo é um conservante natural e as viagens da época iam para a Índia. 
+					   <br><br>Podemos dizer que essa história é bem controversa, há quem defenda mas também há quem considere uma piada. 
+					   <br><br>Fato é que o estilo surgiu na Inglaterra, como uma versão mais potente e amarga do que as pale ales e ele se tornou um coadjuvante até o renascimento da americana. 
+					   <br><br>Nossa IPAiÓ é classica! Assim como a expressão “Ó pai, ó”. Ela vem homenagear e agradecer à Bahia, e  ao seu povo,  que nos recebeu de braços abertos quando chegamos das Minas Gerais. `,
+		'img' : 'ipaio.png'
+
+	},
+	{
+		'nome' : 'Polifônica',
+		'tipo' : 'American IPA',
+		'ingredientes' : 'Água, malte de cevada alemão, malte de cevada belga, lúpulos americanos e levedura',
+		'composicao' : 'ABV 5.9%. - IBU 57',
+		'descricao' : `Se trata da versão mais popular da cerveja IPA, mas podemos te dizer que ela também é subdividida em algumas regiões, aqui no Brasil não ouvimos muito falar sobre isso, já nos Estados Unidos costuma-se ouvir que a American IPA se divide entre duas categorias: East Coast e West Coast (inventada na Califórnia).
+					   <br><br>Duas características marcantes são o amargor que ela apresenta e também o aroma intenso que vem dos lúpulos americanos. 
+					   <br><br>Na Polifônica sentiremos os lúpulos que remetem a frutas tropicais e um leve sabor de graviola. Essas diversas notas de frutas nos fizeram homenagear uma das bandas mais melódicas de Belo Horizonte, Graveola e o Lixo Polifônico. Quando sentir vontade de viajar num mar de notas musicais e poesia, escute-os! Logo logo lembrará de nossa cerveja!`,
+		'img' : ''
+
+	},
+	{
+		'nome' : '4P',
+		'tipo' : 'Dry Stout',
+		'ingredientes' : 'Água, malte de cevada alemão, malte de cevada belga, lúpulos europeus e levedura ',
+		'composicao' : 'ABV 5.1%. - IBU 20',
+		'descricao' : `Dry stout ou Irish stout (em irlandês, leann dubh, "cerveja preta") é um estilo de cerveja bem escura e que muitas vezes tem um sabor "torrado" ou semelhante a café. Enquanto a maior parte das versões comerciais depende primariamente de cevada torrada como grão escuro, outras usam malte chocolate, malte black ou combinações dos três. 
+					   O exemplo mais famoso é a Guinnes. O conteúdo alcoólico e a sensação "seca" (pouco açúcar) de uma stout são ambas caracterizadas como leve, embora varie de país para país. A secura provém do uso da cevada torrada. 
+					   Nossa 4P, “poder para o povo preto” é uma homenagem ao movimento Black Power. Luta , respeito e igualdade são um dos lemas de nossa cervejaria.`,
+		'img' : ''
+
+	},
+	{
+		'nome' : 'Praieiro',
+		'tipo' : 'Classic American Pilsner',
+		'ingredientes' : 'Água, malte de cevada alemão, malte de cevada belga, lúpulos europeus e levedura ',
+		'composicao' : 'ABV 4.3%. - IBU 14',
+		'descricao' : `Uma versão americana da Bohemian Pilsener. Começou a ser produzida nos Estados Unidos por imigrantes que utilizavam amostras das leveduras originais e ingredientes nativos da América. Com o advento da Lei Seca americana, esse estilo acabou se adptando ao paladar local, já que o longo período sem beber cervejas mais elaboradas fez com que os americanos desacostumassem com cervejas de sabores e aromas fortes.  São bastante refrescantes, além de seca, com um amargor leve e um lúpulo que, apesar de ser notado, é pouco agressivo. 
+					   <br><br>Nossa Praieiro foi inspirada na  praia mais popular de Morro de São Paulo, a Segunda Praia!`,
+		'img' : 'praieiro.png'
+
+	},
+	{
+		'nome' : 'Sereias',
+		'tipo' : 'Belgian Blond Ale',
+		'ingredientes' : 'Água, malte de cevada alemão, malte de cevada belga, lúpulos americanos e levedura',
+		'composicao' : 'ABV 6.9%. - IBU 17',
+		'descricao' : `Com o surgimento das cervejas Pilsen, cervejarias do mundo todo procuram alternativas para concorrer com a nova sensação do universo cervejeiro. Na Bélgica, a partir das cervejas de Abadia, criou-se uma cerveja mais clara e menos amarga, a Belgian Blond Ale, que são cervejas complexas, perfumadas e muito cremosas. Há uma grande harmonia entre seu teor alcoólico, sua presença de lúpulo e de malte. Elas têm a cor variando do amarelo palha ao dourado, com espuma branca, densa e duradoura. 
+					   <br><br>Mas a nossa Sereias não é loira! Autêntica, como deve ser qualquer cerveja, nossa versão belga é vermelha! Utilizamos hibisco em nossa receita, trazendo a cor e um sabor cítrico. Mas pra deixa-la única, não fizemos só “colorir” a cerveja. Com uma carbonatação mais elevada, o cítrico do hibisco fica mais ressaltado. 
+					   <br><br>Quer beber e ao mesmo tempo combater os radicais livres e ficar sem reter líquido? A Sereias é a solução! Mas cuidado, é a nossa cerveja com o maior teor alcoólico!`,
+		'img' : ''
+
+	},
+	{
+		'nome' : 'Vila do Morro',
+		'tipo' : 'Bohemian Pilsener',
+		'ingredientes' : 'Água, malte de cevada alemão, malte de cevada belga, lúpulos europeus e levedura ',
+		'composicao' : 'ABV 4.9%. - IBU 35',
+		'descricao' : `O termo “Pilsner” tem origem na cidade de Pilsen na República Tcheca, a cidade produzia cerveja de alta fermentação (ale) desde 1295, porém com grande variação do padrão de sabor e qualidade gerando grande insatisfação dos consumidores que em 1838 esvaziaram os barris em protesto. A cidade então se organizou e um ano depois foi fundada uma cervejaria municipal, hoje a Pilsner Urquell, que deveria produzir cerveja de acordo com o pioneiro estilo Bávaro, fabricar cervejas com leveduras de baixa fermentação e as envelhecer em cavernas para que as tornassem mais claras e duradouras. A cervejaria contratou um experiente mestre cervejeiro Bávaro que em Outubro de 1842 apresentou a primeira versão da Pilsner moderna, já utilizando maltes claros e técnicas novas. 
+					   <br><br>Bohemian Pilsner é um estilo de cerveja de aroma rico e complexo, com notas de malte, “spicy” e florais oriundas do lúpulo Saaz e sem esteres frutados. No sabor o amargor pronunciado (ainda que suave), a característica “spicy” do lúpulo e a presença do malte garantem uma combinação agradável, também não há esteres frutados no sabor. Sua cor varia entre o amarelo palha  e o dourado profundo, tem corpo médio, carbonatação média e colarinho branco, cremoso e persistente. 
+					   <br><br>Nossa Vila foi inspirada na praça central de Morro de São Paulo. Onde existem vários restaurantes com características bem próprias, assim como nossa Vila. Experimente e tente desvendar esses sabores!`,
+		'img' : 'viladomorro.png'
+
+	}
+]
